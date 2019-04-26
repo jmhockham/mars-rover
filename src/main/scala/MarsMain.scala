68063f2c6045541414a0dd39cc6dee20b038b8ba
jmhockham <jmhockham@gmail.com>
2019-04-26 12:15:01 +0100

    Update MarsMain.scala
    Added option to take command line args for input file

--- src/main/scala/MarsMain.scala
+++ src/main/scala/MarsMain.scala
@@ -1,21 +1,26 @@
 import model.{InputData, Rover}
 import service.{InputDataService, RoverService}
 
+import scala.io.Source
 import scala.io.Source._
 
 object MarsMain extends App {
 
-//  println("Hello! Starting rover input")
-//  println("Commands are as follows:")
-  private val commands: Seq[String] = fromResource("input.txt").getLines().toSeq
-//  commands.foreach(println)
+  //  println("Hello! Starting rover input")
+  //  println("Commands are as follows:")
+  private val commands: Seq[String] = if (args.length == 0) {
+    fromResource("input.txt").getLines().toSeq
+  } else {
+    Source.fromFile(args(0)).getLines().toSeq
+  }
+  //  commands.foreach(println)
 
-//  println("Running rover commands...")
+  //  println("Running rover commands...")
   private val rovers: Seq[Rover] = InputDataService.processCommands(commands) match {
     case None => Seq.empty[Rover]
     case Some(inputData) => RoverService.processInstructions(inputData)
   }
-//  println("Rovers status: ")
+  //  println("Rovers status: ")
   rovers.foreach(r => println(r.getStatusSimple()))
 
 }
